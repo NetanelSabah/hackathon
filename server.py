cc2eb0eb588daf4af9d8728a43502548651b2d06
Netanel Sabah <sabahn@post.bgu.ac.il>
2020-12-28 20:25:58 +0200

    working on server and client pt. 1

--- server.py
+++ server.py
@@ -12,7 +12,9 @@ UDP_TIMEOUT = 0.2 #timeout for UDP server
 MAGIC_COOKIE = 0xfeedbeef #UDP header cookie
 TYPE = 0x2 # UDP messagetype
 
-threads = []
+threads = [] # TCP client threads pool
+player_sockets = {} # player sockets pool (dictionary of format name:socket)
+player_sockets_lock = threading.Lock()
 
 class UDPBroadcast(threading.Thread):
 
@@ -23,16 +25,16 @@ class UDPBroadcast(threading.Thread):
     def run(self):
         try:
             # UDP server setup
-            UDPserver = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) #set up UDP
-            UDPserver.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1) #allow multiple clients (important)
-            UDPserver.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1) #broadcast
-            UDPserver.bind(('', SERVER_UDP_PORT))
-            UDPserver.settimeout(UDP_TIMEOUT)
+            UDP_server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) #set up UDP
+            UDP_server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1) #allow multiple clients (important)
+            UDP_server.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1) #broadcast
+            UDP_server.bind(('', SERVER_UDP_PORT))
+            UDP_server.settimeout(UDP_TIMEOUT)
 
             # sending broadcast messages
             message = struct.pack('IBH', MAGIC_COOKIE, TYPE, SERVER_TCP_PORT)  # encode magic cookie, type and TCP port
             for i in range(WAITING_FOR_CLIENT_COUNT): #repeat 10 times
-                UDPserver.sendto(message, ('<broadcast>', SERVER_UDP_PORT))
+                UDP_server.sendto(message, ('<broadcast>', SERVER_UDP_PORT))
                 log("%s) message sent!"%i)
                 time.sleep(1) # wait a second between every broadcast
         except Exception as e:
@@ -44,44 +46,53 @@ class ClientThread(threading.Thread):
         threading.Thread.__init__(self) 
         self.ip = ip 
         self.port = port
-        log("[+] New server socket thread started for " + ip + ":" + str(port))
+        self.name = ''
+        log("New server socket thread started for " + ip + ":" + str(port))
 
     def run(self):
-        while True:
-            try:
-                data = connectionSocket.recv(1048)
-            except Exception as e:
-                print("Client socket %s/%s failed." %(ip, port))
-                err("Error: "+str(e))
-            print ("Server received data: %s"%data)
-            #connectionSocket.send(b"message received")  # echo 
-            if data == b'exit' or data ==  b'':
-                print("closing connection...")
-                break
+        try:
+            data = connectionSocket.recv(1048)
+            result = data.decode('utf8')
+            result_length = len(result)
+            if (result_length > 0 and result[result_length-1] == '\n'): # check if it's a name
+                name = result[:result_length-1]
+                log("player added to pool: %s"%name)
+                player_sockets[name] = connectionSocket 
+            else:
+                print("got an invalid connection message (does not end with newline), closing connection.")
+        except Exception as e:
+            print("Client socket %s/%s failed." %(ip, port))
+            err("Error: "+str(e))
+        log("closing connection with %s/%s..." %(ip, port))
+        player_sockets.pop(connectionSocket)
+        connectionSocket.close()
+
+
+
 
 
 
 try:
     #TCP server socket setup
-    TCPserverIP = socket.gethostbyname(socket.gethostname()) #get the IP for printing the message
+    TCP_server_IP = socket.gethostbyname(socket.gethostname()) #get the IP for printing the message
     server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     server.bind(('', SERVER_TCP_PORT))
     server.listen(1)
-    print("Server started, listening on IP address %s for %s seconds"%(TCPserverIP,WAITING_FOR_CLIENT_COUNT))
+    print("Server started, listening on IP address %s for %s seconds"%(TCP_server_IP,WAITING_FOR_CLIENT_COUNT))
 
     #UDP thread start
-    UDPthread = UDPBroadcast()
-    UDPthread.start()
-
+    UDP_thread = UDPBroadcast()
+    UDP_thread.start()
+    
     #accept users in TCP
     initTime = time.time()
     remainingTime = WAITING_FOR_CLIENT_COUNT + WAITING_FOR_CLIENT_EXTRA # initial remaining time, plus time to catch last requests from UDP broadcasts
     while remainingTime>0: 
         server.settimeout(remainingTime) # the timeout will be the remaining time
         (connectionSocket, (ip, port)) = server.accept()
-        newThread = ClientThread(ip, port)
-        newThread.start()
-        threads.append(newThread)
+        new_thread = ClientThread(ip, port)
+        new_thread.start()
+        threads.append(new_thread)
         remainingTime = WAITING_FOR_CLIENT_COUNT-(time.time()-initTime) #set the remaining time to the server (essentially 10 - time elapsed since start)
 except socket.timeout:
     pass # timeouts are OK.
@@ -92,6 +103,6 @@ print("Finished listening, game start...")
 
 
 for t in threads:
-    t.join()
-    UDPthread.join()
+    threads[t].join()
+UDP_thread.join()
       
